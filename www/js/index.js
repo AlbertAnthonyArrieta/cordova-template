f264e353e60e0b89377afe87a3175b77c80e3bbe
Arrieta <albert.arrieta@uleth.ca>
2022-11-14 11:23:00 -0700

    moving

--- www/js/index.js
+++ www/js/index.js
@@ -4,4 +4,24 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
+    var options = {
+        quality: 80,
+        destinationType: Camera.DestinationType.FILE_URI
+    }
+
+
+    $("#takePhoto").on("click", takePic);
+
+    function takePic() {
+        navigator.camera.getPicture(onSuccess, onError, options);
+    }
+
+    function onSuccess(imageData) {
+        console.log(imageData);
+        $("#myimg").attr("src", imageData);
+    }
+
+    function onError() {
+        alert("Photo not taken because" + message);
+    }
 }
